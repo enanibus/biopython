6eb8e523786e5eddb73dff3a5f4802278f7c6ccd
Jacobo Enriquez <michi_two@yahoo.comn>
2016-10-22 21:45:36 +0200

    Corregido error MySeq en vez de Seq

--- Code_SelfEval_2016.py
+++ Code_SelfEval_2016.py
@@ -1,17 +1,25 @@
 import re
-Seq=""
-MySeq=open("ID1001.fasta","r")
+
+Seq = ""
+MySeq = open("ID1001.fasta", "r")
 for Line in MySeq:
-  if not(">" in Line):
-    Seq=Seq+Line.strip()
+  if not (">" in Line):
+    Seq = Seq + Line.strip()
 MySeq.close()
-MyRE=r"[D,E]-[A,V,L,I]-x(0,1)-{AQWSTCDERF}"
-MyRegex=re.compile(MyRE)
-def SearchAll (Regex,Seq,pos):
-  Res=Regex.search(Seq,pos)
-  if Res==None:
-    return()
+MyRE = r"[D,E]-[A,V,L,I]-x(0,1)-{AQWSTCDERF}"
+MyRegex = re.compile(MyRE)
+
+print("Seq: ", Seq)
+print("Regex: ", MyRegex)
+print(type(Seq))
+
+def SearchAll(Regex, Seq, pos):
+  Res = Regex.search(Seq, pos)
+  if Res == None:
+    return ()
   else:
-    print(Res.group(),"\t",Res.start(),"t",Res.end())
-    SearchAll(Regex,Seq,Res.start()+1)
-SearchAll(MyRegex,MySeq,0)
+    print(Res.group(), "\t", Res.start(), "t", Res.end())
+    SearchAll(Regex, Seq, Res.start() + 1)
+
+
+SearchAll(MyRegex, Seq, 0)
